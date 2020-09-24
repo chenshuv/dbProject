199ae4c989bd482897ecdbbcc80b34682fb6cf5b
chenshuv <yi2105@qq.com>
2020-09-24 20:32:11 +0800

    初始化仓库

---
+++ ch1_20181002928/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public Cat(String name){
+        this.name = name;
+    }
+
+    public void shout(){
+        System.out.printf("miao");
+    }
+}
